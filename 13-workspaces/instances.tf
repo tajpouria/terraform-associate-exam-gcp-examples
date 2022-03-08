c67bb37c13ca575976fad066e9478faaaf551417
tajpouria <pouria.tajdivand@betconstruct.com>
2022-03-08 16:40:22 +0400

    feat: add check ws on gcs multi ws

--- 13-workspaces/instances.tf
+++ 13-workspaces/instances.tf
@@ -2,5 +2,5 @@ module "apache_hello_world" {
   source                = "tajpouria/apache-hello-world/google"
   version               = "0.0.1"
   instance_name         = "test-module"
-  instance_machine_type = "e2-small"
+  instance_machine_type = terraform.workspace == "dev" ? "e2-micro" : "e2-small"
 }
