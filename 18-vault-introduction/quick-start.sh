34d33f2358a8ca8deef7c81a6846b0ff66a94f8a
tajpouria <tajpouria.dev@gmail.com>
2022-03-10 21:54:42 +0400

    feat: add -wip secret engines

--- 18-vault-introduction/quick-start.sh
+++ 18-vault-introduction/quick-start.sh
@@ -1,12 +1,10 @@
+# Starting the server
 vault server -dev
-
 UNSEAL_KEY='DeuQt6ab9Tpvdhr81pxupa72QxCZKBcXn0H7WCJyCes='
 export VAULT_ADDR='http://127.0.0.1:8200' && export VAULT_TOKEN="s.EWKOWADaFBHHvZypf88RAne8"
-
 vault login "$VAULT_TOKEN"
-
 vault status
-
+# First secret
 vault kv -help
 vault kv put -help
 vault kv put secret/hello foo=world
@@ -19,3 +17,12 @@ vault kv undelete -versions=2 secret/hello
 vault kv get secret/hello
 vault kv destroy -versions=2 secret/hello
 vault kv get secret/hello
+# Secret Engines
+vault secrets enable -path=kv kv
+vault secrets list
+vault kv put kv/hello target=hello
+vault kv put kv/my-secret value="secret"
+vault kv get kv/my-secret
+vault kv list kv
+vault kv delete kv/hello
+vault secrets disable kv/
